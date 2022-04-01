0506160d737420f9b66af042defa7bab7270e3b3
DartSkywalker <piskunovalxy@gmail.com>
2022-04-01 10:10:28 +0300

    updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import bs4.element
-
+import xlsxwriter
 from spiders.fastcomet_spider import FastcometSpider
 from scrapy import signals
 from scrapy.crawler import CrawlerProcess
@@ -41,9 +41,10 @@ def extract_data_from_url():
     urls_list = [x.strip() for x in urls_list]
     url_len = len(urls_list)
 
+    data_list = []
+
     # Get data from each url
     for i, url in enumerate(urls_list):
-
         print(f"Processing {i+1}/{url_len}")
 
         # Get title of the url using bs4
@@ -69,18 +70,29 @@ def extract_data_from_url():
         except AttributeError:
             h1 = ''
 
+        data_list.append([url, title, h1])
         # Export result to csv file in format - Page Url, Page Title, H1 tag text
-        with open('data.csv', 'a') as f:
-            try:
-                f.write("%s,%s,%s\n" % (url, title, h1))
-            except UnicodeEncodeError:
-                continue
+        # with open('data.csv', 'a') as f:
+        #     try:
+        #         # f.write("%s,%s,%s\n" % (url, title, h1))
+        #         if "," in h1:
+        #             f.write(f'{url},{title},"{h1}"\n')
+        #         else:
+        #             f.write(f'{url},{title},{h1}\n')
+        #     except UnicodeEncodeError:
+        #         continue
+
+    with xlsxwriter.Workbook('data.xlsx') as workbook:
+        worksheet = workbook.add_worksheet()
+
+        for row_num, data in enumerate(data_list):
+            worksheet.write_row(row_num, 0, data)
 
     print("Data successfully extracted and saved to data_old.csv")
 
 
 def main():
-    # crawl_website()
+    crawl_website()
     extract_data_from_url()
 
 
